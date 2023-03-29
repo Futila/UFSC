ab515347e7f7c89e178305d41278bff22c544b3c
Fernando Futila <fernando.futila@gmail.com>
2023-03-29 09:49:41 -0300

    Estudents names

--- prog-2/t1.cpp
+++ prog-2/t1.cpp
@@ -1,3 +1,7 @@
+// Estudantes
+// Fernando Doqui Futila (22250762)
+// Fabio Augusto Martini Reis (19204589)
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -23,9 +27,11 @@ int main()
     cout << "2. Print index and string" << endl;
     cout << "3. Search string (literal) " << endl;
     cout << "4. Search substrings " << endl;
-    cout << "5. Remove string (by index) " << endl << endl;
+    cout << "5. Remove string (by index) " << endl
+         << endl;
 
-    cout << "0. Quit " << endl << endl;
+    cout << "0. Quit " << endl
+         << endl;
 
     cout << "Choose an option:  ";
 
@@ -60,16 +66,20 @@ int main()
       cout << endl;
       int count = 1;
 
-       for (size_t i = 0; i < wordsVector.size(); i++)
+      for (size_t i = 0; i < wordsVector.size(); i++)
       {
-          if (wordsVector[i] == search_word){
-             cout << count << " palavra <" << search_word << "> encontrada na posicao: "<< i<< endl;
-             count ++;
-          }else {
-            if (i == wordsVector.size() -1) {
-                cout << "Word not found !!!";
-            }
+        if (wordsVector[i] == search_word)
+        {
+          cout << count << " palavra <" << search_word << "> encontrada na posicao: " << i << endl;
+          count++;
+        }
+        else
+        {
+          if (i == wordsVector.size() - 1)
+          {
+            cout << "Word not found !!!";
           }
+        }
       }
 
       break;
@@ -82,20 +92,23 @@ int main()
       cout << endl;
       int count = 1;
 
-       for (size_t i = 0; i < wordsVector.size(); i++)
+      for (size_t i = 0; i < wordsVector.size(); i++)
       {
-          size_t found = wordsVector[i].find(substring);
-          if(found != string::npos){
-          }else {
-            if (i == wordsVector.size() -1) {
-                cout << "Substring not found !!!";
-            }
+        size_t found = wordsVector[i].find(substring);
+        if (found != string::npos)
+        {
+        }
+        else
+        {
+          if (i == wordsVector.size() - 1)
+          {
+            cout << "Substring not found !!!";
           }
+        }
       }
 
       break;
     }
-
 
     case 5:
     {
@@ -103,32 +116,32 @@ int main()
       cin >> idx;
       cout << endl;
 
-      if(idx < 0 || idx > wordsVector.size()-1) {
-        cout << "Invalid index";
-      }else if (wordsVector.empty()){
-        cout << "A lista esta vazia" << endl;
-      }else {
-           wordsVector.erase(wordsVector.begin() + idx);
-
-           cout << "Elemento removido" <<endl <<endl;
-
-           if (!wordsVector.empty()) {
-
-                cout << "Lista atualizada: " << endl <<endl;
-                  for (size_t i = 0; i < wordsVector.size(); i++)
+      if (idx < 0 || idx > wordsVector.size() - 1)
       {
-        cout << "Index[" << i << "] - Word: " << wordsVector[i] << endl;
+        cout << "Invalid index";
       }
-
-           }
-
-
+      else if (wordsVector.empty())
+      {
+        cout << "A lista esta vazia" << endl;
       }
+      else
+      {
+        wordsVector.erase(wordsVector.begin() + idx);
 
+        cout << "Elemento removido" << endl
+             << endl;
 
+        if (!wordsVector.empty())
+        {
 
-
-
+          cout << "Lista atualizada: " << endl
+               << endl;
+          for (size_t i = 0; i < wordsVector.size(); i++)
+          {
+            cout << "Index[" << i << "] - Word: " << wordsVector[i] << endl;
+          }
+        }
+      }
 
       break;
     }
@@ -146,7 +159,7 @@ int main()
     }
     }
 
-  } while (opcao != 10);
+  } while (opcao != 0);
 
   return 0;
 }
